8678bc5358ecf624ab7cc4041c1d4f35c9238d61
Dan Tashman <dantashman@gmail.com>
2013-05-02 14:58:38 -0400

    created virgil.js
    replace title script

---
+++ Virgil-Mobile/js/virgil.js
@@ -0,0 +1,16 @@
+var $title = $('h2');
+var input  = document.getElementsById ('#title-input');
+var $name1 = $('#row1');
+var $name2 = $('#row2');
+var $name3 = $('#row3');
+var $name4 = $('#row4');
+var $name5 = $('#row5');
+var $name6 = $('#row6');
+var $name7 = $('#row7');
+var $name8 = $('#row8');
+var $name9 = $('#row9');
+
+function titleSwap() {
+	document.replace.InnerHTML(input)
+	
+};
